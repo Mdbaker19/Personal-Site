d2e9f8350018d76862ba81b4d75f7afb784d80d9
Matthew <bakebill20@yahoo.com>
2020-11-19 11:25:33 -0600

    added layout for projects area

--- personal.js
+++ personal.js
@@ -3,6 +3,13 @@ $(document).ready(function (){
        $(".heading").css("display", "flex");
    });
 
-
+    $(".homeSection").on("click", function (){
+        let currentText = $(this).text();
+        let nextText = $(this).next().text();
+        let prevText = $(this).prev().text();
+        console.log("current text is: " + currentText);
+        console.log("next text is: " + nextText);
+        console.log("prev text is: " + prevText);
+    });
 
 });
